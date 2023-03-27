9c99aaaf414472f2afd480b0879b7756c0308425
Joshua Cavell <xclusive36@gmail.com>
2023-03-27 18:14:48 -0400

    added features to generate readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Project Title
+
+## Description
+
+## Badge
+![https://img.shields.io/badge/license-MIT-blue.svg](https://img.shields.io/badge/license-MIT-blue.svg)
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [Badges](#badges)
+## Installation
+
+## Usage
+
+## License
+[MIT](https://api.github.com/licenses/mit)
+## Contributing
+
+## Tests
+
+## Questions
+Profile: [Github Profile](https://github.com//)  
+Additional questions: [Email](mailto:)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,90 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenses = [
+  {
+    key: "MIT",
+    url: "https://api.github.com/licenses/mit",
+  },
+  {
+    key: "LGPL",
+    url: "https://api.github.com/licenses/lgpl-3.0",
+  },
+  {
+    key: "MPL",
+    url: "https://api.github.com/licenses/mpl-2.0",
+  },
+  {
+    key: "AGPL",
+    url: "https://api.github.com/licenses/agpl-3.0",
+  },
+  {
+    key: "Apache",
+    url: "https://api.github.com/licenses/apache-2.0",
+  },
+  {
+    key: "GPL",
+    url: "https://api.github.com/licenses/gpl-3.0",
+  },
+];
 
-// TODO: Create a function that returns the license link
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    return `![https://img.shields.io/badge/license-${license}-blue.svg](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  } else {
+    return "";
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license) {
+    const licenseObj = licenses.find(
+      (licenseObj) => licenseObj.key === license
+    );
+    return `[${license}](${licenseObj.url})`;
+  } else {
+    return "";
+  }
+}
+
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
   console.log("Greetings from generateMarkdown.js!");
   return `# ${data.title}
+
+## Description
+${data.description}
+## Badge
+${renderLicenseBadge(data.license)}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [Badges](#badges)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${renderLicenseLink(data.license)}
+## Contributing
+${data.guidelines}
+## Tests
+${data.test}
+## Questions
+Github Profile: [Github Profile](https://github.com/${data.username}/)  
+Additional questions: [Email](mailto:${data.email})
 `;
 }
 
 module.exports = generateMarkdown;
+
+// https://shields.io/category/license
